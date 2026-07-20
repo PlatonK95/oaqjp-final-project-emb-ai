8e85cf6aca38c3c11c58702823b7ea823bc94fb8
PlatonK95 <platwnaskouroupis@gmail.com>
2026-07-20 08:00:50 -0400

    Achieved 9.23/10 pylint score for server.py

--- server.py
+++ server.py
@@ -1,18 +1,27 @@
+"""
+Executing this function initiates the application of emotion
+detection to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
+
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    Analyze the text provided in the request parameters for emotion detection
+    and return the formatted result string or error message.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-    
+
     response = emotion_detector(text_to_analyze)
-    
-    # Έλεγχος αν το dominant_emotion είναι None
+
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    
+
     return (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
@@ -21,9 +30,14 @@ def sent_analyzer():
         f"The dominant emotion is {response['dominant_emotion']}."
     )
 
+
 @app.route("/")
 def render_index_page():
+    """
+    Render the main application index page.
+    """
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
